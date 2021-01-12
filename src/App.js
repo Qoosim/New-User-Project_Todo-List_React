abc4aa42978ab772e7fbc5fda538e8558b33d8de
Qoosim <ayinde.abdulghaniyy@gmail.com>
2021-01-12 16:01:43 +0100

    Create table to structure todo list

--- src/App.js
+++ src/App.js
@@ -1,18 +1,31 @@
 import React, { Component } from 'react'
 
 class App extends Component {
+
     constructor() {
         super()
         this.state = {
-            userName: 'Qoosim'
+            userName: 'Qoosim', 
+            todoItems: [
+                { action: 'Buy Milk', done: false },
+                { action: 'Dentist at 5pm', done: false },
+                { action: 'Go to Gym', done: false }
+            ]
         }
     }
 
-    changeStateData = () => {
-        this.setState({
-            userName: this.state.userName === 'Qoosim' ? 'Ayinde' : 'Qoosim'
-        })
-    }
+    //changeStateData = () => {
+     //   this.setState({
+      //      userName: this.state.userName === 'Qoosim' ? 'Ayinde' : 'Qoosim'
+       // })
+    //}
+
+    todoRows = () => 
+        this.state.todoItems.map((item) => (
+            <tr key={item.action}>
+                <td>{item.action}</td>
+            </tr>
+        ))
 
     render() {
         return (
@@ -22,11 +35,22 @@ class App extends Component {
                         <h2 className="bg-primary text-white text-center p2">
                             {this.state.userName} Todo List
                         </h2>
-                        <button className='btn btn-secondary m-2' onClick={this.changeStateData}>Change
-
+                        <button 
+                            className='btn btn-secondary m-2' 
+                            onClick={this.changeStateData}>
+                            Change
                         </button>
                     </div>
-
+                    <div className="col-12">
+                        <table className="table">
+                            <thead>
+                                <tr>
+                                    <th>Task</th>
+                                </tr>
+                            </thead>
+                            <tbody>{this.todoRows()}</tbody>
+                        </table>
+                    </div>
                 </div>
 
             </div>
